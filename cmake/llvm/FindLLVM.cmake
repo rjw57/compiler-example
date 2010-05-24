60f30526b8c732fd31f1ab09d1983fc1c57220b8
Rich Wareham <rjw57@cam.ac.uk>
2010-05-24 11:03:22 +0100

    Documentation fixes for FindLLVM.cmake along with support for retrieving the LLVM version found.

--- cmake/llvm/FindLLVM.cmake
+++ cmake/llvm/FindLLVM.cmake
@@ -1,4 +1,26 @@
-# Find LLVM - Find the LLVM includes and library.
+# - Find LLVM 
+# This module can be used to find LLVM.
+# It requires that the llvm-config executable be available on the system path.
+# Once found, llvm-config is used for everything else.
+#
+# Typical usage could be:
+#   find_package(LLVM QUIET REQUIRED COMPONENTS jit native interpreter)
+#
+# If the QUIET flag is not set, the specified components and LLVM version are
+# outputted.
+#
+# If the COMPONENTS are not set, the default set of "all" is used.
+#
+# The following variables are set:
+#
+# LLVM_FOUND        - Set to YES if LLVM is found.
+# LLVM_VERSION      - Set to the decimal version of the LLVM library.
+# LLVM_C_FLAGS      - All flags that should be passed to a C compiler. 
+# LLVM_CXX_FLAGS    - All flags that should be passed to a C++ compiler.
+# LLVM_CPP_FLAGS    - All flags that should be passed to the C pre-processor.
+# LLVM_LIBRARY_DIRS - A list of directories where the LLVM libraries are located.
+# LLVM_INCLUDE_DIRS - A list of directories where the LLVM headers are located.
+# LLVM_LIBRARIES    - A list of libraries which should be linked against.
 
 # A macro to run llvm config
 macro(_llvm_config _var_name)
@@ -39,12 +61,17 @@ if(NOT LLVM_FIND_QUIETLY)
 	message(STATUS "Looking for LLVM components: ${_llvm_components}")
 endif(NOT LLVM_FIND_QUIETLY)
 
+_llvm_config(LLVM_VERSION --version)
 _llvm_config(LLVM_C_FLAGS --cflags ${_llvm_components})
 _llvm_config(LLVM_CXX_FLAGS --cxxflags ${_llvm_components})
 _llvm_config(LLVM_CPP_FLAGS --cppflags ${_llvm_components})
 _llvm_config(LLVM_LIBRARY_DIRS --libdir ${_llvm_components})
 _llvm_config(LLVM_INCLUDE_DIRS --includedir ${_llvm_components})
 _llvm_config(LLVM_LIBRARIES --libs ${_llvm_components})
+
+if(NOT LLVM_FIND_QUIETLY)
+	message(STATUS "Found LLVM version: ${LLVM_VERSION}")
+endif(NOT LLVM_FIND_QUIETLY)
 
 # handle the QUIETLY and REQUIRED arguments and set LLVM_FOUND to TRUE if 
 # all listed variables are TRUE
